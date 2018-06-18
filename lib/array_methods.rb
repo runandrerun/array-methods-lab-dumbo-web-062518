9743d6f173ae77bb8fa8cc179b4d4bc0cc8d386a
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-18 18:35:11 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,17 +9,21 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-
+  reversed = array.reverse
+  return reversed
 end
 
 def using_first(array)
-
+  first = array.first
+  return first
 end
 
 def using_last(array)
-
+  last = array.last
+  return last
 end
 
 def using_size(array)
-
+  size = array.size
+  return size
 end
